9de857aeae17eb6c7cc9252343e65229a600f23f
João Almeida <176084410+04ramalmeida@users.noreply.github.com>
2024-09-02 12:34:02 +0100

    Add roles

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -51,7 +51,7 @@ namespace SuperShop.Controllers
         }
 
         // GET: Products/Create
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
             return View();
