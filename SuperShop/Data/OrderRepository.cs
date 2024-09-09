b95827c83fa71ee782f8e18aafcb34decf0e8607
João Almeida <176084410+04ramalmeida@users.noreply.github.com>
2024-09-09 01:45:14 +0100

    Add items on detail temp

--- SuperShop/Data/OrderRepository.cs
+++ SuperShop/Data/OrderRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SuperShop.Data.Entities;
 using SuperShop.Helpers;
+using SuperShop.Models;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +17,42 @@ namespace SuperShop.Data
             _userHelper = userHelper;
         }
 
+        public async Task AddItemToOrderAsync(AddItemViewModel model, string userName)
+        {
+            var user = await _userHelper.GetUserByEmailAsync(userName);
+            if (user == null)
+            {
+                return;
+            }
+
+            var product = await _context.Products.FindAsync(model.ProductId);
+            if (product == null)
+            {
+                return;
+            }
+            
+            var orderDetailTemp = await _context.OrderDetailsTemp
+                .Where(odt => odt.User == user && odt.Product == product)
+                .FirstOrDefaultAsync();
+            if (orderDetailTemp == null) 
+            {
+                orderDetailTemp = new OrderDetailTemp 
+                {
+                    Price = product.Price,
+                    Product = product,
+                    Quantity = model.Quantity,
+                    User = user
+                };
+                _context.OrderDetailsTemp.Add(orderDetailTemp);
+            }
+            else
+            {
+                orderDetailTemp.Quantity += model.Quantity;
+                _context.OrderDetailsTemp.Update(orderDetailTemp);
+            }
+            await _context.SaveChangesAsync();
+        }
+
         public async Task<IQueryable<Order>> GetOrderAsync(string userName)
         {
             var user = await _userHelper.GetUserByEmailAsync(userName);
@@ -29,7 +66,7 @@ namespace SuperShop.Data
                 return _context.Orders
                     .Include(o => o.Items)
                     .ThenInclude(i => i.Product)
-                    .Include(o => o.OrderDate);
+                    .OrderByDescending(o => o.OrderDate);
             }
 
             return _context.Orders
@@ -51,6 +88,21 @@ namespace SuperShop.Data
                 .Include(p => p.Product)
                 .Where(o => o.User == user)
                 .OrderBy(o => o.Product.Name);
+        }
+
+        public async Task ModifyOrderDetailTempQuantityAsync(int id, double quantity)
+        {
+            var orderDetailTemp = await _context.OrderDetailsTemp.FindAsync(id);
+            if (orderDetailTemp == null) 
+            {
+                return;
+            }
+            orderDetailTemp.Quantity += quantity;
+            if (orderDetailTemp.Quantity > 0)
+            {
+                _context.OrderDetailsTemp.Update(orderDetailTemp);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
