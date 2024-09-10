1661debfdb5e93ccd8842d78784c90299177a2f8
João Almeida <176084410+04ramalmeida@users.noreply.github.com>
2024-09-10 00:38:21 -0700

    Add delivery functionality

--- SuperShop/Data/OrderRepository.cs
+++ SuperShop/Data/OrderRepository.cs
@@ -104,6 +104,19 @@ namespace SuperShop.Data
             await _context.SaveChangesAsync();
         }
 
+        public async Task DeliverOrder(DeliveryViewModel model)
+        {
+            var order = await _context.Orders.FindAsync(model.Id);
+            if (order == null)
+            {
+                return;
+            }
+
+            order.DeliveryDate = model.DeliveryDate;
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+        }
+
         public async Task<IQueryable<Order>> GetOrderAsync(string userName)
         {
             var user = await _userHelper.GetUserByEmailAsync(userName);
@@ -126,6 +139,11 @@ namespace SuperShop.Data
                 .ThenInclude(p => p.Product)
                 .Where(o => o.User == user)
                 .OrderByDescending(o => o.OrderDate);
+        }
+
+        public async Task<Order> GetOrderAsync(int id)
+        {
+            return await _context.Orders.FindAsync(id);
         }
 
         public async Task<IQueryable<OrderDetailTemp>> GetOrderDetailTempsAsync(string userName)
