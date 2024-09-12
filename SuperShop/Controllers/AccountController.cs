29290ffba045d53fc1239574a78ec234256c1396
João Almeida <176084410+04ramalmeida@users.noreply.github.com>
2024-09-12 02:03:56 +0100

    Cascade drop down list

--- SuperShop/Controllers/AccountController.cs
+++ SuperShop/Controllers/AccountController.cs
@@ -5,16 +5,21 @@ using SuperShop.Helpers;
 using SuperShop.Models;
 using System.Linq;
 using System.Threading.Tasks;
+using SuperShop.Data;
 
 namespace SuperShop.Controllers
 {
     public class AccountController : Controller
     {
         private readonly IUserHelper _userHelper;
-
-        public AccountController(IUserHelper userHelper)
+        private readonly ICountryRepository _countryRepository;
+        
+        public AccountController(
+            IUserHelper userHelper,
+            ICountryRepository countryRepository)
         {
             _userHelper = userHelper;
+            _countryRepository = countryRepository;
         }
 
         public IActionResult Login()
@@ -54,7 +59,12 @@ namespace SuperShop.Controllers
 
         public IActionResult Register()
         {
-            return View();
+            var model = new RegisterNewUserViewModel
+            {
+                Countries = _countryRepository.GetComboCountries(),
+                Cities = _countryRepository.GetComboCities(0),
+            };
+            return View(model);
         }
 
         [HttpPost]
@@ -63,12 +73,17 @@ namespace SuperShop.Controllers
             var user = await _userHelper.GetUserByEmailAsync(model.Username);
             if (user == null)
             {
+                var city = await _countryRepository.GetCityAsync(model.CityId);
                 user = new User
                 {
                     FirstName = model.FirstName,
                     LastName = model.LastName,
                     Email = model.Username,
-                    UserName = model.Username
+                    UserName = model.Username,
+                    Address = model.Address,
+                    PhoneNumber = model.PhoneNumber,
+                    CityID = model.CityId,
+                    City = city
                 };
 
                 var result = await _userHelper.AddUserAsync(user, model.Password);
@@ -105,7 +120,26 @@ namespace SuperShop.Controllers
             {
                 model.FirstName = user.FirstName;
                 model.LastName = user.LastName;
+                model.Address = user.Address;
+                model.PhoneNumber = user.PhoneNumber;
+                
+                var city = await _countryRepository.GetCityAsync(user.CityID);
+                if (city != null)
+                {
+                    var country = await _countryRepository.GetCountryAsync(city);
+                    if (country != null)
+                    {
+                        model.CountryId = country.Id;
+                        model.Cities = _countryRepository.GetComboCities(country.Id);
+                        model.Countries = _countryRepository.GetComboCountries();
+                        model.CityId = user.CityID;
+                    }
+                }
             }
+
+            model.Cities = _countryRepository.GetComboCities(model.CountryId);
+            model.Countries = _countryRepository.GetComboCountries();
+            
             return View(model);
         }
 
@@ -115,8 +149,14 @@ namespace SuperShop.Controllers
             var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
             if (user != null)
             {
+                var city = await _countryRepository.GetCityAsync(model.CityId);
+                
                 user.FirstName = model.FirstName;
                 user.LastName = model.LastName;
+                user.Address = model.Address;
+                user.PhoneNumber = model.PhoneNumber;
+                user.CityID = model.CityId;
+                user.City = city;
                 var response = await _userHelper.UpdateUserAsync(user);
                 if (response.Succeeded)
                 {
@@ -165,5 +205,14 @@ namespace SuperShop.Controllers
         {
             return View();
         }
+    
+        [HttpPost]
+        [Route("Account/GetCitiesAsync")]
+        public async Task<IActionResult> GetCitiesAsync(int countryId)
+        {
+            var country = await _countryRepository.GetCountryWithCitiesAsync(countryId);
+            return Json(country.Cities.OrderBy(c => c.Name));
+        }
+        
     }
 }
